a864efecdf125f3f94b3f079038a2b9c9c9ab1a9
Zakhriddinov <zaxriddinovsardor21@gmail.com>
2022-12-06 15:19:26 +0500

    deplo

--- server.js
+++ server.js
@@ -12,5 +12,15 @@ connectDB();
 require("./startup/logging")();
 require("./startup/routes")(app);
 
+const path = require("path");
+if (process.env.NODE_ENV === "production") {
+   app.use(express.static(path.join(__dirname, "../frontend/build")));
+   app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html")));
+} else {
+   app.get("/", (req, res) => {
+      res.json({ message: "API running..." });
+   })
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, winston.info(`Server started on port ${PORT}`));

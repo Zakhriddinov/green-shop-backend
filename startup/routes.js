fdb8bc0d436ba09c252f50522c12cf3f4d3a9985
Zakhriddinov <zaxriddinovsardor21@gmail.com>
2022-12-20 13:51:53 +0500

    a

--- startup/routes.js
+++ startup/routes.js
@@ -4,7 +4,7 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const { errorHandler } = require("../middleware/errorMiddleware");
-
+const jwt = require("jsonwebtoken");
 module.exports = function (app) {
    app.use(bodyParser.json());
    app.use(bodyParser.urlencoded({ extended: false }))
@@ -16,6 +16,16 @@ module.exports = function (app) {
    app.use(morgan('tiny'));
 
    // routes:
+   app.get('/api/v1/get-token', (req, res) => {
+      try {
+         const accessToken = req.headers.authorization.split(' ')[1]
+         const decoted = jwt.verify(accessToken, process.env.JWT_SECRET_KEY)
+         return res.json({ token: decoted.email, isAdmin: decoted.isAdmin })
+      } catch (error) {
+         return res.status(401).send("Unauthorized. Invalid Token");
+      }
+   });
+
    app.use("/api/v1/products", require("../routes/productRoutes"));
    app.use("/api/v1/users", require("../routes/userRoutes"));
    app.use("/api/v1/categories", require("../routes/categoryRoutes"));
